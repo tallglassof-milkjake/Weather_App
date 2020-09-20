1920a933f5d52a1f33fa4adde9ecc0ac0ea19661
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-09-20 23:13:26 +0930

    updating manipulated html to include all nexessary info except uv index

--- main.js
+++ main.js
@@ -16,11 +16,17 @@ $(document).ready(function() {
     
             response.main.temp = response.main.temp - 273.15;
             let celsius = response.main.temp.toFixed(2);
+            response.main.feels_like = response.main.feels_like - 273.25;
+            let feelsLikeCelsius = response.main.feels_like.toFixed(2);
 
             let currentWeatherDiv = $('#current-weather');
             
             currentWeatherDiv.append('<h1 class="display-3">' + response.name + '</h1>');
             currentWeatherDiv.append('<p class="lead">Temperature: ' + celsius + '</p>');
+            currentWeatherDiv.append('<p class="lead">Feels Like: ' + feelsLikeCelsius + '</p>');
+            currentWeatherDiv.append('<p class="lead">Humidity: ' + response.main.humidity + '%</p>')
+            currentWeatherDiv.append('<p class="lead">Wind Speed: ' + response.wind.speed + '</p>')
+            //currentWeatherDiv.append('<p class="lead">UV Index: ' + response.wind.speed + '</p>')
         });
     
         
